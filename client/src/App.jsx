4e86cc400bc3bb23f58e397eae99d334108ac62a
Anand <anandsingh14442@gmail.com>
2025-07-21 22:50:31 +0530

    added copyright

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
-import Navbar from './components/Navbar'
 import { AuthProvider } from './context/AuthContext';
 
 const App = () => {
   return (
-    <AuthProvider>               {/* ✅ Auth context wrapper */}
-      <BrowserRouter>           {/* ✅ Router wrapper */}
+    <AuthProvider>
+      <BrowserRouter>
         <AppRoutes />
+
+        {/* ✅ Copyright */}
+        <div className="fixed bottom-2 right-2 text-xs text-gray-500 bg-white/80 px-3 py-1 rounded-md shadow-sm z-50">
+          © {new Date().getFullYear()} TradePro by Anand Singh
+        </div>
       </BrowserRouter>
     </AuthProvider>
   );
